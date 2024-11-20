c023750c6174b7afe8e7eb498b30e3710a31abfc
Milad Mohammadi <miladmohammadi_1993@yahoo.com>
2024-11-20 18:09:03 +0330

    fixed work flow

--- public/js/main.js
+++ public/js/main.js
@@ -6409,7 +6409,7 @@ limitations under the License.
           }
 
           var query = $(this).val();
-          var searchPage = 'https://elugiran.github.io/elugiran.github.io/search/?q=' + query;
+          var searchPage = 'https://elugiran.github.io/search/?q=' + query;
           document.location = searchPage;
 
           return false;
